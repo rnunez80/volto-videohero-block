409df08a45e67dd1c86e0b15bd2b402dc4ddf1ae
rnunez <rnunez@york.cuny.edu>
2024-08-22 20:26:59 -0400

    optimize for better performance without affecting SSR, refactor to use @loadable/component instead of React.lazy

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-import React, { Suspense } from 'react';
+import React from 'react';
+import loadable from '@loadable/component';
 import iconSVG from '@plone/volto/icons/video.svg';
+import { Loader } from 'semantic-ui-react';
 
-// Use React.lazy to dynamically import the components
-const View = React.lazy(() => import('./View'));
-const Edit = React.lazy(() => import('./Edit'));
+// Use @loadable/component to dynamically import the components
+const View = loadable(() => import('./View'), {
+  fallback: (
+    <div>
+      <Loader active inline='centered' />
+      <p>Loading...</p>
+    </div>
+  ),
+});
+
+const Edit = loadable(() => import('./Edit'), {
+  fallback: (
+    <div>
+      <Loader active inline='centered' />
+      <p>Loading...</p>
+    </div>
+  ),
+});
 
 const applyConfig = (config) => {
   config.blocks.blocksConfig.videohero = {
@@ -11,16 +28,8 @@ const applyConfig = (config) => {
     title: 'Video Hero',
     icon: iconSVG,
     group: 'common',
-    view: (props) => (
-      <Suspense fallback={<div>Loading...</div>}>
-        <View {...props} />
-      </Suspense>
-    ),
-    edit: (props) => (
-      <Suspense fallback={<div>Loading...</div>}>
-        <Edit {...props} />
-      </Suspense>
-    ),
+    view: View,
+    edit: Edit,
     restricted: false,
     mostUsed: true,
     sidebarTab: 1,
