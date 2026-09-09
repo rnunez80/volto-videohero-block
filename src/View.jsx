78f8a86846f8eb1d534cf5916dc100fc6c4fcc1e
rnunez <rnunez@york.cuny.edu>
2026-09-09 12:12:43 -0400

    pagespeed optimization and removal of unnecessary imports

--- src/View.jsx
+++ src/View.jsx
@@ -45,7 +45,7 @@ const VideoHero = ({ data }) => {
             defaultMuted={true}
             aria-hidden="true"
             tabIndex="-1"
-            fetchpriority="high"
+            preload="none"
           >
             {data.size === 'full' ? (
               <>
